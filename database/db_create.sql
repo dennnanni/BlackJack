88954917bdb54fbbfcf8eabc60af83b8822584a8
Denise Nanni <denise.nanni1@gmail.com>
2025-05-12 15:51:06 +0200

    added SQL script for db creation and updated ORM

---
+++ database/db_create.sql
@@ -0,0 +1,23 @@
+DROP DATABASE IF EXISTS BlackJack;
+CREATE DATABASE BlackJack;
+\c BlackJack
+
+CREATE TABLE user (
+    username TEXT PRIMARY KEY,
+    password TEXT NOT NULL,
+    salt TEXT NOT NULL,
+    balance NUMERIC(10, 2) DEFAULT 0.0
+);
+
+CREATE TABLE gameserver (
+    id SERIAL PRIMARY KEY,
+    ip TEXT NOT NULL,
+    port INT NOT NULL,
+    key TEXT NOT NULL
+);
+
+CREATE TABLE userserver (
+    username TEXT REFERENCES user(username) ON DELETE CASCADE,
+    idserver INT REFERENCES gameserver(id) ON DELETE CASCADE,
+    PRIMARY KEY (username, idserver)
+);
--- main_server/database_interface/orm/orm.py
+++ main_server/database_interface/orm/orm.py
@@ -5,16 +5,17 @@ Base = declarative_base()
 
 # Associazione molti-a-molti tra User e GameServer
 userservers = Table(
-    'userservers', Base.metadata,
-    Column('username', String, ForeignKey('users.username', ondelete='CASCADE'), primary_key=True),
+    'userserver', Base.metadata,
+    Column('username', String, ForeignKey('user.username', ondelete='CASCADE'), primary_key=True),
     Column('idserver', Integer, ForeignKey('gameserver.id', ondelete='CASCADE'), primary_key=True)
 )
 
 class User(Base):
-    __tablename__ = 'users'
+    __tablename__ = 'user'
 
     username = Column(String, primary_key=True)
     password = Column(String, nullable=False)
+    salt = Column(String, nullable=False)
     balance = Column(Float, default=0.0)
 
     servers = relationship("GameServer", secondary=userservers, back_populates="users")
