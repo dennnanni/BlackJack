eb6553ab115893b37ab644424d6a10dad0e0f472
Denise Nanni <denise.nanni1@gmail.com>
2025-11-30 14:18:13 +0100

    initial structure of central server

---
+++ central_server/central_server/__init__.py
@@ -0,0 +1,46 @@
+from client.model.structures import UserSession
+from flask import Flask, redirect
+from flask_socketio import SocketIO
+from flask_login import LoginManager
+from cryptography.fernet import Fernet
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+SHARED_SECRET = os.getenv('SHARED_SECRET').encode()
+if not SHARED_SECRET:
+    raise ValueError('SHARED_SECRET environment variable not set')
+fernet_shared_secret = Fernet(SHARED_SECRET)
+
+DATABASE_URL = os.getenv('DATABASE_URL')
+if not DATABASE_URL:
+    raise ValueError('DATABASE_URL environment variable is not set')
+
+socketio = SocketIO(cors_allowed_origins="*", manage_session=False)
+
+def create_app():
+    app = Flask(__name__)
+    app.config['SECRET_KEY'] = 'secret!'
+    app.config['SESSION_PERMANENT'] = False
+    
+    from .routes import register_routes
+    register_routes(app)
+    
+    from .event_handlers import register_event_handlers
+    register_event_handlers(socketio)
+
+    login_manager = LoginManager()
+    login_manager.init_app(app)
+    login_manager.login_view = 'client_interface.login'
+    
+    @login_manager.user_loader
+    def load_user(username):
+        return UserSession(username)
+
+    @login_manager.unauthorized_handler
+    def unauthorized():
+        return redirect('/login') 
+    
+    socketio.init_app(app)
+    return app
