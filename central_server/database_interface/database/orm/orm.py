a668aa1e3aef9a3171085a4c9406c0f736ac4232
Denise Nanni <denise.nanni1@gmail.com>
2026-08-12 15:01:56 +0200

    fix(central): restore orm file mistakenly deleted

---
+++ central_server/database_interface/database/orm/orm.py
@@ -0,0 +1,31 @@
+from sqlalchemy import Column, String, Integer, ForeignKey, Table, Numeric
+from sqlalchemy.orm import declarative_base, relationship
+
+Base = declarative_base()
+
+# Associazione molti-a-molti tra User e GameServer
+userservers = Table(
+    'userserver', Base.metadata,
+    Column('username', String, ForeignKey('user.username', ondelete='CASCADE'), primary_key=True),
+    Column('idserver', Integer, ForeignKey('gameserver.id', ondelete='CASCADE'), primary_key=True)
+)
+
+class User(Base):
+    __tablename__ = 'user'
+
+    username = Column(String, primary_key=True)
+    password = Column(String, nullable=False)
+    salt = Column(String, nullable=False)
+    balance = Column(Numeric(10,2), default=0.0)
+
+    servers = relationship("GameServer", secondary=userservers, back_populates="users")
+
+class GameServer(Base):
+    __tablename__ = 'gameserver'
+
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    ip = Column(String, nullable=False)
+    port = Column(Integer, nullable=False)
+    key = Column(String, nullable=False)
+
+    users = relationship("User", secondary=userservers, back_populates="servers")
