2f6a6cfa4fab0b06f622e440a8f72b50a36fc952
Denise Nanni <denise.nanni1@gmail.com>
2026-09-24 16:40:12 +0200

    fix(central): secrets only reside in .env

--- secret_generator.py
+++ secret_generator.py
@@ -3,7 +3,9 @@ import secrets
 def main():
     with open('.env', 'w') as f:
         f.write(f'SHARED_SECRET={secrets.token_urlsafe(32)}\n')
+
         f.write(f'GAME_SECRET_KEY={secrets.token_urlsafe(32)}\n')
+        f.write(f'SECRET_KEY={secrets.token_urlsafe(32)}\n')
 
 if __name__ == "__main__":
     main()
